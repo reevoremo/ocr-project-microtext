895b739f3b780790e60e32ec524cf7cb6fd338ca
Behan Remoshan <behanremoshan.benetruban@epita.fr>
2016-10-30 17:33:45 +0100

    new H file

--- MicroText/neural-net.c
+++ MicroText/neural-net.c
@@ -2,51 +2,10 @@
 //Testing// IF YOU EDIT THE CODE PUT YOUR NAME NEXT TO IT 
 //#include <vector>
 #include <stdlib.h>
-
-struct Neuron;
-typedef Neuron Layer[] ;
-
-struct Net
-{
-public:
-	Net(const unsigned topology[]  &topology);
-	void feedForward(const double inputVals[] &inputVals);
-	void backProp(const double targetVals[]  &targetVals);
-	void getResults(double resultVals[]  &resulVals ) const;
-
-private:
-	Layer  m_layers[]; //m_layers[layerNum][neuronNum]	
-};
-
-Net::Net(const vector<unsigned> &topology)
-{
-	unsigned numLayer = topology.size();
-	for(unsigned layerNum = 0; layerNum < numLayers; ++layerNum)
-	{
-		m_layers.push_back(Layer());
-		for (unsigned neuronNUm =0; neuronNum <= topology <= topology[layerNum];++neuronNum)
-		{
-			m_layers.back().push_back(Neuraon());	
-		}
-	}
-}
+#include "neural-net.h"
 
 
 int main()
 {
-	unsigned topology[];
-	topology.push_back(3);
-	topology.push_back(2);
-	topology.push_back(1);
-	Net myNet(topology);
-	
-	vector<double> inputVals;
-	myNet.feedForward(inputVals);
-
-	vector<double> targetVals;
-	myNet.backProp(targetVals);
-
-	vector<double> resultVals;
-	myNet.getResults(resultVals);	
-	
+		
 }
