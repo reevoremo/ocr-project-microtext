2d9b9124752a1e1f4815f37bca66db22582558f1
Behan Remoshan BENET RUBAN <behanremoshan.benetruban@epita.fr>
2016-11-18 20:42:29 +0100

    ZET is the new start

---
+++ MicroText/zet-neural-net.c
@@ -0,0 +1,22 @@
+//Static XOR gate training PLEAE WORK//
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+#define DEBUG 1
+
+#define NUM_TRAINING 4
+#define NUM_INPUTS 2
+#define NUM_HIDDEN 2
+#define NUM_OUTPUTS 1
+
+#define sigmoid(x)	(1.0/(1.0 + (float)exp(-(double)(x))))
+#define sigmoidPrime(x)	((float)(x) * (1.0 - (x)))
+
+int main()
+{
+
+return 0;
+}
+
