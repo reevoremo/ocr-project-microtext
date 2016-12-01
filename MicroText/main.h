12803733cb1771d9ba3e960d82740331d8e41b2a
Pierre Ruth <pierre.ruth@epita.fr>
2016-12-01 16:11:20 +0100

    Fixed main.h

--- MicroText/main.h
+++ MicroText/main.h
@@ -3,11 +3,14 @@
  
 # include <stdlib.h>
 # include <SDL.h>
+# include <gtk/gtk.h>
 
 void save_characters(Img_array characters, char *file_name);
 
 char* concat(const char *s1, const char *s2);
 
 void char_to_matrix(SDL_Surface *chara, char *save);
+
+int make_interface(int argc, char **argv);
 
 # endif
